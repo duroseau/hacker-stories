9f4c6d9dad019f88d294dc371e673dc2c3ac9682
eddy duroseau <eddyduroseau@gmail.com>
2021-03-03 15:00:13 -0500

    App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function getTitle(title) {
-  return title;
-}
 
 function App() {
-
-
   return (
-    <div> 
-      <h1>Hello {getTitle('React')}</h1>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo'/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.oeg'
+          target='_blank'
+          rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+      </header>
     </div>
-  );
+  )
 }
 
 export default App;
