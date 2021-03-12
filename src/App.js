1a829ceaa8ec634cee16058c24edddb5d0a94a69
eddy duroseau <eddyduroseau@gmail.com>
2021-03-12 00:10:54 -0500

    hacker-stories

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React from 'react';
 const list = [
   {
     title: 'React',
-    url: 'htpps//reactjs.org/',
+    url: 'https://reactjs.org/',
     author: 'Jordan Walke',
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
-    title: 'Redux',
+    title:'Redux',
     url: 'https://redux.js.org/',
     author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
@@ -18,33 +18,31 @@ const list = [
     objectID: 1,
   },
 ];
+function App() { 
+ return (
+   <div>
+     <h1>My Hacker Stories</h1>
 
-function App() {
-  return (
-    <div>
-      ...
+     <label htmlFor="search">Search: </label>
+     <input id="search" type="text" />
 
-      <hr />
-      {/* <h1>My Hacker Stories</h1> */}
+     <hr />
 
-      {/* <label htmlFor="search">Search: </label>
-      <input id="search" type="text" /> */}
-
-      
-
-      {list.map(function(item) {
-        return <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          {item.title}
-          </div>;
-      })}
-    </div>
-  );
+     {list.map(function(item) {
+       return (
+         <div key={item.objectID}>
+           <span>
+             <a href={item.url}>{item.title}</a>
+           </span>
+           <span>{item.author}</span>
+           <span>{item.num_comments}</span>
+           <span>{item.points}</span>
+         {item.title}
+         </div>
+       )
+     })}  
+   </div>
+     
+     );
 }
-
  export default App;
