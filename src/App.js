fe0355cbf4769af9d4f37b65621bde0eaa01aa86
eddy duroseau <eddyduroseau@gmail.com>
2021-03-23 22:43:23 -0400

    hacker-stories

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title:'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ]
   const handleChange = event => {
     console.log(event.target.value);
   }
@@ -12,7 +30,7 @@ const App = () => {
      <input id="search" type="text" onChange={handleChange} />
 
      <hr />
-     <List />
+     <List list={stories}/>
     </div>
   );
 };
@@ -61,9 +79,9 @@ const list = [
      
 //      );
 // }
-function List() {
-  return list.map(function(item) {
-    return (
+const List = props =>
+  props.list.map(item => (
+    
       <div key={item.objectID}>
         <span>
           <a href={item.url}>{item.title}</a>
@@ -72,9 +90,9 @@ function List() {
         <span>{item.num_comments}</span>
         <span>{item.points}</span>
       </div>
-    );
-  });
-}
+    ));
+  
+
 
 // function App() {
 //   return (
